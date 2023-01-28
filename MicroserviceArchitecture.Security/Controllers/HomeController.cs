d9d9bb19e7e2462fcbe5777b10bac1419f658d5b
Jose Andres Hurtado <joseandreshurtadov@gmail.com>
2023-01-28 15:11:08 -0400

    Add Consul to Security microservice.

---
+++ MicroserviceArchitecture.Security/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceArchitecture.Security.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping() => Ok();
+    }
+}
--- MicroserviceArchitecture.Security/Program.cs
+++ MicroserviceArchitecture.Security/Program.cs
@@ -1,4 +1,7 @@
+using Aforo255.Cross.Discovery.Consul;
+using Aforo255.Cross.Discovery.Mvc;
 using Aforo255.Cross.Token.Src;
+using Consul;
 using MicroserviceArchitecture.Security.Repositories;
 using MicroserviceArchitecture.Security.Services;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +21,10 @@ builder.Services.AddScoped<IAccessService, AccessService>();
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("jwt"));
 
+builder.Services.AddSingleton<IServiceId, ServiceId>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddConsul();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,4 +33,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var serviceId = app.UseConsul();
+var consulClient = app.Services.GetRequiredService<IConsulClient>();
+app.Lifetime.ApplicationStopped.Register(() =>
+{
+    consulClient.Agent.ServiceDeregister(serviceId);
+});
+
 app.Run();
+
