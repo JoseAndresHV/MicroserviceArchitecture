c7f988ddb6b765e9fdea84e4e52c64046c0b5f5c
Jose Andres Hurtado <joseandreshurtadov@gmail.com>
2023-01-28 15:19:12 -0400

    Add Consul to Account microservice.

---
+++ MicroserviceArchitecture.Account/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceArchitecture.Account.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping() => Ok();
+    }
+}
--- MicroserviceArchitecture.Account/Program.cs
+++ MicroserviceArchitecture.Account/Program.cs
@@ -1,3 +1,6 @@
+using Aforo255.Cross.Discovery.Consul;
+using Aforo255.Cross.Discovery.Mvc;
+using Consul;
 using MicroserviceArchitecture.Account.Repositories;
 using MicroserviceArchitecture.Account.Services;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +19,10 @@ builder.Services.AddDbContext<ContextDatabase>(
 
 builder.Services.AddScoped<IAccountService, AccountService>();
 
+builder.Services.AddSingleton<IServiceId, ServiceId>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddConsul();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +30,12 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var serviceId = app.UseConsul();
+var consulClient = app.Services.GetRequiredService<IConsulClient>();
+app.Lifetime.ApplicationStopped.Register(() =>
+{
+    consulClient.Agent.ServiceDeregister(serviceId);
+});
 
 app.Run();
